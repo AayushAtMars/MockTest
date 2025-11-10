ef76487768c7a4c93b84e218dce7e97f9ca055e3
AayushAtMars <yoaayush14@gmail.com>
2025-11-10 13:10:36 +0530

    x2qwe

--- Screens/Profile.jsx
+++ Screens/Profile.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Profile = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Profile Page</Text>
+      <Text style={styles.text}>Profile</Text>
     </View>
   )
 }
